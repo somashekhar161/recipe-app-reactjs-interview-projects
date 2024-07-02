e197576409397decaf49e20b2511263c9503352e
Somashekar B R <sanjayrao161@gmail.com>
2024-07-03 01:06:26 +0530

    error check 3

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+// import App from "./App.jsx";
 import "./index.css";
-import { RecipeProvider } from "./context/Recipe/RecipeContext.jsx";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Home from "./pages/Home";
-import Favorites from "./pages/Favorites";
-import Details from "./pages/Details";
-import ErrorPage from "./pages/ErrorPage";
+// import { RecipeProvider } from "./context/Recipe/RecipeContext.jsx";
+// import { RouterProvider, createBrowserRouter } from "react-router-dom";
+// import Home from "./pages/Home";
+// import Favorites from "./pages/Favorites";
+// import Details from "./pages/Details";
+// import ErrorPage from "./pages/ErrorPage";
 // const router = createBrowserRouter(
 //   [
 //     {
