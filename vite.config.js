f7783dab24d4fb77a4821675977064ad611445ef
Somashekar B R <sanjayrao161@gmail.com>
2024-07-03 01:24:52 +0530

    error check 6

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/recipe-app-reactjs-interview-projects/",
+  base: "/weather-app-reactjs-interview-projects/",
 });
